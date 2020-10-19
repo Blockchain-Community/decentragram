98b6e6b35ce19efb1cc3a047c7c294811fa79333
sangya2001 <sangyasherpa2058@gmail.com>
2020-10-19 19:59:54 +0545

    Configured package file

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -4,8 +4,20 @@ contract Decentragram {
   string public name = "Decentragram";
 
   // Store Images
+  mapping(uint => Image) public images;
+
+  struct Image{
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
 
   // Create Images
+  function uploadImage() public {
+    images[1] = Image(1, 'abc123', 'Hello, world', 0, address(0x0));
+  }
 
-  //Tip Images
+  // Tip Images
 }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const Decentragram = artifacts.require('./Decentragram.sol')
 
+// chai is used for tesing
 require('chai')
   .use(require('chai-as-promised'))
   .should()
@@ -7,12 +8,16 @@ require('chai')
 contract('Decentragram', ([deployer, author, tipper]) => {
   let decentragram
 
+  // before method is used to initiate the contract
   before(async () => {
     decentragram = await Decentragram.deployed()
   })
 
+  // describe gives the title for what we are doing
   describe('deployment', async () => {
+    // it method checks the specific task
     it('deploys successfully', async () => {
+      // check if the address is valid
       const address = await decentragram.address
       assert.notEqual(address, 0x0)
       assert.notEqual(address, '')
@@ -20,9 +25,20 @@ contract('Decentragram', ([deployer, author, tipper]) => {
       assert.notEqual(address, undefined)
     })
 
+    // checks if the name is retrieved successfully
     it('has a name', async () => {
       const name = await decentragram.name()
       assert.equal(name, 'Decentragram')
+    })
+  })
+
+  describe('images', async () => {
+    let result
+
+    it('create images', async () => {
+      result = await decentragram.uploadImage()
+      let image  = await decentragram.images(1)
+      console.log(image)
     })
   })
 })
